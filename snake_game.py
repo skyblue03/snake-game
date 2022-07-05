a548bf75cae1c13e6d9ac1a4aeb1dfc1d9b01174
skyblue03 <ehsan.hasani@outlook.com.au>
2022-07-05 15:38:00 +0930

    Pause and resume functionality

--- snake_game.py
+++ snake_game.py
@@ -46,6 +46,9 @@ clock = pygame.time.Clock()
 # Score variable
 score = 0
 
+# Pause flag
+paused = False
+
 def show_score(color, font, size):
     score_font = pygame.font.SysFont(font, size)
     score_surface = score_font.render('Score: ' + str(score), True, color)
@@ -81,97 +84,108 @@ while running:
                 change_to = 'LEFT'
             elif event.key == pygame.K_RIGHT and direction != 'LEFT':
                 change_to = 'RIGHT'
+            elif event.key == pygame.K_ESCAPE:  # Pause/Resume game
+                paused = not paused
     
     direction = change_to
 
-    # Move the snake
-    if direction == 'UP':
-        snake_pos[1] -= 10
-    elif direction == 'DOWN':
-        snake_pos[1] += 10
-    elif direction == 'LEFT':
-        snake_pos[0] -= 10
-    elif direction == 'RIGHT':
-        snake_pos[0] += 10
+    if not paused:
+        # Move the snake
+        if direction == 'UP':
+            snake_pos[1] -= 10
+        elif direction == 'DOWN':
+            snake_pos[1] += 10
+        elif direction == 'LEFT':
+            snake_pos[0] -= 10
+        elif direction == 'RIGHT':
+            snake_pos[0] += 10
 
-    # Snake body growing mechanism
-    snake_body.insert(0, list(snake_pos))
-    if snake_pos == food_pos:
-        food_spawn = False
-        score += 10  # Increase score by 10 for normal food
-    else:
-        snake_body.pop()
-    
-    # Food spawn
-    if not food_spawn:
-        food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
-        food_spawn = True
-
-    # Spawn special food occasionally
-    if not special_food_effect:
-        if random.randint(1, 100) <= speed_boost_chance:  # Speed boost food
-            speed_boost_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
-            special_food_effect = 'speed'
-        elif random.randint(1, 100) <= slow_down_chance:  # Slow down food
-            slow_down_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
-            special_food_effect = 'slow'
-        elif random.randint(1, 100) <= shrink_chance:  # Shrink food
-            shrink_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
-            special_food_effect = 'shrink'
-
-    # Handle eating special food
-    if special_food_effect == 'speed' and snake_pos == speed_boost_food_pos:
-        effect_duration = 50  # Number of frames the effect lasts
-        speed_boost_food_pos = []
-        snake_speed = 30  # Increased speed
-        special_food_effect = False
-
-    elif special_food_effect == 'slow' and snake_pos == slow_down_food_pos:
-        effect_duration = 50
-        slow_down_food_pos = []
-        snake_speed = 10  # Decreased speed
-        special_food_effect = False
-
-    elif special_food_effect == 'shrink' and snake_pos == shrink_food_pos:
-        effect_duration = 0  # No duration, instant effect
-        shrink_food_pos = []
-        if len(snake_body) > 3:  # Ensure the snake doesn't shrink below a minimum length
+        # Snake body growing mechanism
+        snake_body.insert(0, list(snake_pos))
+        if snake_pos == food_pos:
+            food_spawn = False
+            score += 10  # Increase score by 10 for normal food
+        else:
             snake_body.pop()
-        special_food_effect = False
+        
+        # Food spawn
+        if not food_spawn:
+            food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
+            food_spawn = True
 
-    # Apply effects
-    if effect_duration > 0:
-        effect_duration -= 1
-    else:
-        snake_speed = 15  # Normal speed
+        # Spawn special food occasionally
+        if not special_food_effect:
+            if random.randint(1, 100) <= speed_boost_chance:  # Speed boost food
+                speed_boost_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
+                special_food_effect = 'speed'
+            elif random.randint(1, 100) <= slow_down_chance:  # Slow down food
+                slow_down_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
+                special_food_effect = 'slow'
+            elif random.randint(1, 100) <= shrink_chance:  # Shrink food
+                shrink_food_pos = [random.randrange(1, (width // 10)) * 10, random.randrange(1, (height // 10)) * 10]
+                special_food_effect = 'shrink'
 
-    # Game Over conditions
-    if snake_pos[0] < 0 or snake_pos[0] >= width or snake_pos[1] < 0 or snake_pos[1] >= height:
-        game_over()
-    for block in snake_body[1:]:
-        if snake_pos == block:
+        # Handle eating special food
+        if special_food_effect == 'speed' and snake_pos == speed_boost_food_pos:
+            effect_duration = 50  # Number of frames the effect lasts
+            speed_boost_food_pos = []
+            snake_speed = 30  # Increased speed
+            special_food_effect = False
+
+        elif special_food_effect == 'slow' and snake_pos == slow_down_food_pos:
+            effect_duration = 50
+            slow_down_food_pos = []
+            snake_speed = 10  # Decreased speed
+            special_food_effect = False
+
+        elif special_food_effect == 'shrink' and snake_pos == shrink_food_pos:
+            effect_duration = 0  # No duration, instant effect
+            shrink_food_pos = []
+            if len(snake_body) > 3:  # Ensure the snake doesn't shrink below a minimum length
+                snake_body.pop()
+            special_food_effect = False
+
+        # Apply effects
+        if effect_duration > 0:
+            effect_duration -= 1
+        else:
+            snake_speed = 15  # Normal speed
+
+        # Game Over conditions
+        if snake_pos[0] < 0 or snake_pos[0] >= width or snake_pos[1] < 0 or snake_pos[1] >= height:
             game_over()
+        for block in snake_body[1:]:
+            if snake_pos == block:
+                game_over()
 
-    # Refresh game screen
-    screen.fill(black)
+        # Refresh game screen
+        screen.fill(black)
 
-    # Draw snake
-    for pos in snake_body:
-        pygame.draw.rect(screen, green, pygame.Rect(pos[0], pos[1], 10, 10))
-    
-    # Draw food
-    pygame.draw.rect(screen, red, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
-    
-    # Draw special food
-    if special_food_effect == 'speed':
-        pygame.draw.rect(screen, blue, pygame.Rect(speed_boost_food_pos[0], speed_boost_food_pos[1], 10, 10))
-    elif special_food_effect == 'slow':
-        pygame.draw.rect(screen, yellow, pygame.Rect(slow_down_food_pos[0], slow_down_food_pos[1], 10, 10))
-    elif special_food_effect == 'shrink':
-        pygame.draw.rect(screen, orange, pygame.Rect(shrink_food_pos[0], shrink_food_pos[1], 10, 10))
+        # Draw snake
+        for pos in snake_body:
+            pygame.draw.rect(screen, green, pygame.Rect(pos[0], pos[1], 10, 10))
+        
+        # Draw food
+        pygame.draw.rect(screen, red, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
+        
+        # Draw special food
+        if special_food_effect == 'speed':
+            pygame.draw.rect(screen, blue, pygame.Rect(speed_boost_food_pos[0], speed_boost_food_pos[1], 10, 10))
+        elif special_food_effect == 'slow':
+            pygame.draw.rect(screen, yellow, pygame.Rect(slow_down_food_pos[0], slow_down_food_pos[1], 10, 10))
+        elif special_food_effect == 'shrink':
+            pygame.draw.rect(screen, orange, pygame.Rect(shrink_food_pos[0], shrink_food_pos[1], 10, 10))
 
-    # Display score
-    show_score(white, 'times new roman', 20)
+        # Display score
+        show_score(white, 'times new roman', 20)
+    else:
+        # Display pause message
+        pause_font = pygame.font.SysFont('times new roman', 50)
+        pause_surface = pause_font.render('Game Paused', True, red)
+        pause_rect = pause_surface.get_rect()
+        pause_rect.midtop = (width / 2, height / 2)
+        screen.blit(pause_surface, pause_rect)
+        pygame.display.flip()
 
     # Update display
     pygame.display.update()
